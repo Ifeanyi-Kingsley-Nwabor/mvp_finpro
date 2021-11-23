e7c3afc1516b59e95595b35064013e76cde9fb89
Codify KING <81686044+Ifeanyi-Kingsley-Nwabor@users.noreply.github.com>
2021-11-23 17:09:35 +0100

    add services routes

--- app.js
+++ app.js
@@ -2,27 +2,35 @@
 require("dotenv").config();
 const createError = require('http-errors');
 const express = require('express');
+const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const
-const indexRouter = require('./routes/index');
+const helmet = require("helmet");
+const cors = require('cors')
+
+const servicesRouter = require('./routes/services');
 const usersRouter = require('./routes/users');
-const
-const app = express();
+
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+app.use(cors())
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/services', servicesRouter);
+app.use('/api/users', usersRouter);
+
+app.get('/', (req, res, next) => {
+  res.json('welcome to mvp_finpro')
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/services.js
+++ routes/services.js
@@ -1,9 +1,80 @@
 var express = require('express');
-var router = express.Router();
+const db = require('../database/db');
+var servicesRouter = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// servicesRouter.get('/', (req, res, next) =>{
+//   res.render('index', { title: 'Express' });
+// });
+
+servicesRouter.get('/', (req, res, next) => {
+  db.query(`SELECT * FROM services ORDER BY id ASC`)
+  .then((data) => res.json(data.rows))
+  .catch(next);
 });
 
-module.exports = router;
+servicesRouter.get('/:id', ( req, res, next) => {
+  const {id} = req.params;
+  const service = { 
+      text : "SELECT * FROM services WHERE id = $1",
+      values: [id],
+  }
+  db.query(service)
+  .then((data) => res.status(200).json(data.rows))
+  .catch(next);
+});
+servicesRouter.post('/', (req, res, next) => {
+  const { user_id, title, image, category, price, description } = req.body;
+
+  const newService = {
+      text: `INSERT INTO services (user_id, title, image, category, price, description)
+      VALUES($1,$2,$3,$4,$5,$6)
+      RETURNING *
+      `,
+      values: [user_id, title, image, category, price, description],
+  }
+  db.query(newService)
+  .then((data) => res.status(201).json(data.rows))
+  .catch(next);
+})
+
+servicesRouter.put('/:id', (req, res, next) => {
+  const { user_id, title, image, category, price, description } = req.body;
+  const {id} = req.params;
+  const updateService = {
+      text: `
+      UPDATE services
+      SET user_id=$1, title=$2, image=$3, category=$4, price=$5, description=$6
+      WHERE id=$7
+      RETURNING *
+      `,
+      values:[user_id, title, image, category, price, description, id]
+  }
+  db.query(updateService)
+  .then((data) => res.status(201).json(data.rows))
+  .catch(next);
+})
+
+servicesRouter.delete('/:id', (req, res, next) => {
+  const {id} = req.params;
+  const deleteService = {
+      text:`
+      DELETE FROM services
+      WHERE id=$1
+      RETURNING *`,
+      values: [id],
+  }
+  db.query(deleteService)
+  .then((data) => {
+      if(!data.rows.length) {
+          res.status(404).send(`The requested service does not exist!! Please checkout our latest available services.`)
+      } else {
+          res.status(200).json(data.rows)
+      }
+  })
+  .catch(next);
+});
+
+
+
+module.exports = servicesRouter;
